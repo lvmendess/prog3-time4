ebe0db5206716b4a2e174625b0f3d3a9bd99f4fa
Luísa Duarte <131483413+luisads4@users.noreply.github.com>
2024-04-02 10:05:54 -0300

    Classe Usuário pronta

--- Usuario.java
+++ Usuario.java
@@ -17,11 +17,81 @@ public class Usuario{
         this.genero = genero;
         this.mediaDeLikes = mediaDeLikes;
         this.engajamento = engajamento;
+    }
+    
+    public int getRanking() {
+        return this.ranking;
+    }
 
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
     }
 
     public int getQuantidadePosts() {
-        
+        return this.quantidadePosts;
     }
-}
 
+    public void setQuantidadePosts(int quantidadePosts) {
+        this.quantidadePosts = quantidadePosts;
+    }
+
+    public int getSeguidores() {
+        return this.seguidores;
+    }
+
+    public void setSeguidores(int seguidores) {
+        this.seguidores = seguidores;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getMediaDeLikes() {
+        return this.mediaDeLikes;
+    }
+
+    public void setMediaDeLikes(double mediaDeLikes) {
+        this.mediaDeLikes = mediaDeLikes;
+    }
+
+    public double getEngajamento() {
+        return this.engajamento;
+    }
+
+    public void setEngajamento(double engajamento) {
+        this.engajamento = engajamento;
+    }
+    
+    public void imprimirTudo(){
+        System.out.println("Ranking:" + ranking);
+        System.out.println("Posts: "+ quantidadePosts);
+        System.out.println("Seguidores: "+ seguidores);
+        System.out.println("Nome: "+ nome);
+        System.out.println("Categoria: "+ categoria);
+        System.out.println("Gênero: "+ genero);
+        System.out.println("Média de Likes: "+ mediaDeLikes);
+        System.out.println("Engajamento: "+ engajamento);
+    }
+
+}
