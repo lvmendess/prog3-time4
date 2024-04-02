b32d6a028bccd1e9f350a6b89221ca4c53b6d72b
Lívia Mendes <95483204+lvmendess@users.noreply.github.com>
2024-04-02 10:16:28 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Principal{
+    private LeitorTxt leitor = new LeitorTxt();
+
+    public static void main(String[] args) throws IOException{
+        
+    }
+}
