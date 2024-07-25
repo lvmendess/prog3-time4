31b0982b1da151858bfb480b1c669de12660eaae
luisads4 <0079275@IFMG.EDU.BR>
2024-07-25 08:44:53 -0300

    feat: criamos a classe Tweeter e o get

---
+++ Tweeter.java
@@ -0,0 +1,21 @@
+public class Tweeter {
+
+    private ListaSEIL tweeterList;
+    public String annotatorId;
+
+    public Tweeter(String annotatorId){
+        this.annotatorId = annotatorId;
+        tweeterList = new ListaSEIL();
+    }
+    public String getAnnotatorId() {
+        return annotatorId;
+    }
+    
+    public int tweetCounter(){
+        int aux;
+        while(){}
+    }
+        
+}
+    
+
