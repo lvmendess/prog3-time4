72995aa36d7ec270e8f340a5da255998e447d39f
luisds4 <luisaduarte.ti@gmail.com>
2024-08-01 08:29:56 -0300

    método getTweetList aplicado em Tweeter

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main{
     public static void main(String[] args) throws IOException{
         Analysis analysis = new Analysis();
         analysis.initialize();
-        analysis.list.getVolumeByLang();
-        //analysis.tweeterList.print(); /*BUG: printing "AnnotatorID" instead of the actual ID after it prints 47 (Spanish) and before 62 (Swedish) */
+        //analysis.list.getVolumeByLang();
+        analysis.tweeterList.print(); /*BUG: printing "AnnotatorID" instead of the actual ID after it prints 47 (Spanish) and before 62 (Swedish) */
         /*analysis.mostNegativeLanguage();
         analysis.mostPositiveLanguage();
         analysis.totalVolNegative();
