a12c334d01429df758fed05d1148356b588684cb
lvmendess <liviamendes1703@gmail.com>
2024-09-13 11:09:52 -0300

    método de pesquisa

--- BinaryTree.java
+++ BinaryTree.java
@@ -5,15 +5,31 @@ public class BinaryTree {
         this.root = null;
     }
 
-    /*TODO: conferir se word já existe na árvore - criar método exists(Word w)
-     * se existe: acessar lista de linhas da word presente na árvore e adicionar linha
-     * se não: adicionar word à árvore
+    /*TODO: conferir se word já existe na árvore 
+        - criar método exists(Word w) - DONE
+        - adicionar verificação de existência (chamar método exists()) ao método de inserção - TO-DO
+            * se existe: acessar lista de linhas da word presente na árvore e adicionar linha
+            * se não: adicionar word à árvore
      */
 
-    private Word exists(Word w){
-        Word e = null;
+    public Word exists(Word w){
+        return wordExists(w, root);
+    }
 
-        return e;
+    private Word wordExists(Word w, Word current){ //testar, não sei se funciona
+        if(current==null){ //se chegar em nó terminal/folha
+            return null;
+        }else if(current.value.equals(w.value)){ //palavra à inserir é igual à raiz
+            return current;
+        }else{
+            if(current.value.compareTo(w.value)>0){//procura na sub-árvore esquerda
+                return wordExists(w, current.left);
+            }else if(current.value.compareTo(w.value)<0){//procura na sub-árvore direita
+                return wordExists(w, current.right);
+            }else{
+                return null;
+            }
+        }
     }
     
     private Word insertNew(Word newNode, Word current) {
