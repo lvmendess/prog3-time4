bfb015aebf8524be89fb43b43d6dfb7c804b252b
PedroHL19 <pedrohlopes1903@gmail.com>
2024-10-06 16:31:33 -0300

    tp finished

--- Main.java
+++ Main.java
@@ -11,15 +11,15 @@ public class Main {
     public static void main(String args[]) {
         // Cria instâncias da classe Control para todo os arquivos. A classe Control cria uma árvore e um ReadTxt para trabalhar o arquivo.
         Control control = new Control();
-        control.initialize("C:\\Users\\user\\Documents\\BSI\\2024\\prog3\\tree\\prog3-time4\\dataset\\faroeste.txt");
+        control.initialize("dataset\\faroeste.txt");
         control.printTree();
         control.saveTreeToTxt("saida_faroeste.txt");
 
-        // System.out.println();
+        System.out.println();
 
-        /*Control control2 = new Control();
+        Control control2 = new Control();
         control2.initialize("dataset\\brasil.txt");
         control2.printTree();
-        control2.saveTreeToTxt("saida_brasil.txt");*/
+        control2.saveTreeToTxt("saida_brasil.txt");
     }
 }
